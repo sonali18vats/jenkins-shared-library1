b02e8404bd811145c7ded8aaa557de158dcf189f
sonali18vats <91622359+sonali18vats@users.noreply.github.com>
2025-07-14 13:58:29 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,14 +1,16 @@
-def call(String project, String hubUser) {
+def call(String project, String hubUser, String ImageTag = "latest") {
     sh "docker image build -t ${hubUser}/${project} ."
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
+
     withCredentials([usernamePassword(
-            credentialsId: "docker_cred",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
+        credentialsId: "docker_cred",
+        usernameVariable: "USER",
+        passwordVariable: "PASS"
     )]) {
         sh "docker login -u '$USER' -p '$PASS'"
     }
+
     sh "docker image push ${hubUser}/${project}:${ImageTag}"
     sh "docker image push ${hubUser}/${project}:latest"
 }
