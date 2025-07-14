d2e5af2f86ea9560d9d9b14a40659c1ea640887c
sonali18vats <91622359+sonali18vats@users.noreply.github.com>
2025-07-14 14:45:40 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,18 +1,22 @@
+// vars/dockerBuild.groovy
+
 def call(String project, String hubUser, String ImageTag = "latest") {
     sh "docker image build -t ${hubUser}/${project} ."
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
 
     withCredentials([usernamePassword(
-    credentialsId: 'docker_cred',
-    usernameVariable: 'USER',
-    passwordVariable: 'PASS'
-)]) {
-    sh """
-        echo Logging into DockerHub...
-        echo \$PASS | docker login -u \$USER --password-stdin
-        docker image push ${hubUser}/${project}:${ImageTag}
-        docker image push ${hubUser}/${project}:latest
-    """
+        credentialsId: 'docker_cred',
+        usernameVariable: 'USER',
+        passwordVariable: 'PASS'
+    )]) {
+        sh """
+            echo Logging into DockerHub...
+            echo \$PASS | docker login -u \$USER --password-stdin
+            docker image push ${hubUser}/${project}:${ImageTag}
+            docker image push ${hubUser}/${project}:latest
+        """
+    } // ✅ Closing brace for withCredentials
 }
+
 
