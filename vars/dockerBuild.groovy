47da604fed08ea787123e989b8a3982e9467ba83
sonali18vats <91622359+sonali18vats@users.noreply.github.com>
2025-07-14 14:28:57 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,16 +1,21 @@
+// vars/dockerBuild.groovy
+
 def call(String project, String hubUser, String ImageTag = "latest") {
+
     sh "docker image build -t ${hubUser}/${project} ."
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
 
     withCredentials([usernamePassword(
-        credentialsId: "docker-hub",
-        usernameVariable: "sonali1897",
-        passwordVariable: "Krishna18@"
+        credentialsId: "docker-hub",        // This must match your Jenkins saved credentials ID
+        usernameVariable: 'sonali1897',            // These variables are ONLY available inside the block
+        passwordVariable: 'PASS'
     )]) {
-        sh "docker login -u '$USER' -p '$PASS'"
+        sh """
+            echo Logging into Docker...
+            docker login -u "$USER" -p "$PASS"
+            docker image push ${hubUser}/${project}:${ImageTag}
+            docker image push ${hubUser}/${project}:latest
+        """
     }
-
-    sh "docker image push ${hubUser}/${project}:${ImageTag}"
-    sh "docker image push ${hubUser}/${project}:latest"
 }
