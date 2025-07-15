e3c7fcae260c9532354ade7d3d643ab4a7993b95
sonali18vats <91622359+sonali18vats@users.noreply.github.com>
2025-07-15 19:12:21 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,17 @@
-def call(Map stageParams) {
- 
+def call(Map stageParams = [:]) {
+    if (!stageParams.branch || !stageParams.url) {
+        error "Missing required parameters: 'branch' and/or 'url'"
+    }
+
     checkout([
         $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
+        branches: [[name: stageParams.branch]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [],
+        submoduleCfg: [],
+        userRemoteConfigs: [[
+            url: stageParams.url,
+            credentialsId: stageParams.get('credentialsId', '') // Optional
+        ]]
     ])
-  }
+}
